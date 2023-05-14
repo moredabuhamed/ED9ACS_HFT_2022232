10c477c66d40e6240e64d35c0b77bb9540e60e4c
MURID ABUHAMED <abuhamedmored@gmail.com>
2023-05-14 14:34:09 +0200

    Update Fans.cs

--- ED9ACS_HFT_2022232 Models/Fans.cs
+++ ED9ACS_HFT_2022232 Models/Fans.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ED9ACS_HFT_2022232_Models
 {
-    internal class Fans
+    [Table("fans")]
+
+    public class Fans
     {
+        public Fans()
+        {
+            this.Reservations = new HashSet<Reservations>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string City { get; set; }
+
+
+
+        [Required]
+        public int PhoneNumber { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Email { get; set; }
+
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual ICollection<Reservations> Reservations { get; }
+
+
+        public override string ToString()
+        {
+            return $"\n{this.Id,3} | {this.Name,-20} {this.Email,-28} {this.PhoneNumber,10}  \t {this.City}";
+        }
     }
 }
