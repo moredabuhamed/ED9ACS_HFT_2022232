9f358ac288b11c9c0103151af0070f9c360ee6fb
MURID ABUHAMED <abuhamedmored@gmail.com>
2023-05-14 18:12:01 +0200

    DATA

---
+++ ED9ACS_HFT_2022232 Data/TalkWithYourFavoriteArtistDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ED9ACS_HFT_2022232_Data
+{
+    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C:\Users\Mored\Documents\GitHub\ED9ACS_HFT_2022232\ED9ACS_HFT_2022232 Data\Database1.mdf";Integrated Security=True
+    public class TalkWithYourFavoriteArtistDbContext : DbContext
+    {
+
+    }
+}
