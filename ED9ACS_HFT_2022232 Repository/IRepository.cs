47ca6bc9cb634855ac34deab42406f6311e4078c
MURID ABUHAMED <abuhamedmored@gmail.com>
2023-05-14 20:22:54 +0200

    Update IRepository.cs

--- ED9ACS_HFT_2022232 Repository/IRepository.cs
+++ ED9ACS_HFT_2022232 Repository/IRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace ED9ACS_HFT_2022232_Repository
+﻿using System.Linq;
+
+namespace ED9ACS_HFT_2022232_Repository
 {
     public interface IRepository<T> where T : class
     {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+        void Add(T entity);
+        void Delete(T entity);
     }
 }
