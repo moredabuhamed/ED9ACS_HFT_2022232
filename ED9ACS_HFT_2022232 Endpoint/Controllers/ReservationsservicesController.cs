6a9c38499773bf1dd69c68043bdaf163007ec930
MURID ABUHAMED <abuhamedmored@gmail.com>
2023-05-18 20:32:17 +0200

    change

---
+++ ED9ACS_HFT_2022232 Endpoint/Controllers/ReservationsservicesController.cs
@@ -0,0 +1,49 @@
+﻿using ED9ACS_HFT_2022232_Logic;
+using ED9ACS_HFT_2022232_Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ED9ACS_HFT_2022232_Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReservationsservicesController : ControllerBase
+    {
+        IReservationsServicesLogic RSL;
+
+        public ReservationsservicesController(IReservationsServicesLogic rSL)
+        {
+            RSL = rSL;
+        }
+        // GET: /reservationsservices
+        [HttpGet]
+        public IEnumerable<ReservationsServices> Get()
+        {
+            return RSL.GetAllConnections();
+        }
+
+
+        // GET /reservationsservices/5
+        [HttpGet("{id}")]
+        public ReservationsServices Get(int id)
+        {
+            return RSL.GetConnection(id);
+        }
+
+        // POST /reservationsservices
+        [HttpPost]
+        public void Post([FromBody] ReservationsServices value)
+        {
+            RSL.AddNewConnection(value);
+        }
+
+
+
+        // DELETE /reservationsservices/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            RSL.DeleteConnection(id);
+        }
+    }
+}
--- ED9ACS_HFT_2022232 Endpoint/Startup.cs
+++ ED9ACS_HFT_2022232 Endpoint/Startup.cs
@@ -3,37 +3,61 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ED9ACS_HFT_2022232_Endpoint.services;
+using ED9ACS_HFT_2022232_Logic;
+using ED9ACS_HFT_2022232_Models;
+using ED9ACS_HFT_2022232_Data;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ED9ACS_HFT_2022232_Repository;
 
 namespace ED9ACS_HFT_2022232_Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+            services.AddControllers();
+            ////services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            ////                                      options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            services.AddTransient<IFansLogic, FansLogic>();
+            services.AddTransient<IArtistsLogic, ArtistsLogic>();
+            services.AddTransient<IReservationsLogic, ReservationsLogic>();
+            services.AddTransient<IReservationsServicesLogic, ReservationsServicesLogic>();
+            services.AddTransient<IServicesLogic, ServicesLogic>();
+            services.AddTransient<IFansRepository, FansRepository>();
+            services.AddTransient<IArtistsRepository, ArtistsRepository>();
+            services.AddTransient<IReservationsRepository, ReservationsRepository>();
+            services.AddTransient<IReservationsServicesRepository, ReservationsServicesRepository>();
+            services.AddTransient<IServicesRepository, ServicesRepository>();
+            services.AddTransient<TalkWithYourFavoriteArtistDbContext, TalkWithYourFavoriteArtistDbContext>();
+            services.AddSignalR();
+
+
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(x => x
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins("http://localhost:23079"));
 
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+                endpoints.MapHub<SignalRHub>("/hub");
             });
         }
     }
